fc09795ffe78514f24fcd02684879e26e4b6ef09
bao <thebao.developer@gmail.com>
2023-04-18 15:28:02 +0700

    fix: convert app experimental to page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
+	reactStrictMode: true,
 };
 
-const withMDX = require('@next/mdx')();
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
