a7e23ff60974a915fbb73fcd3f52b2e0e06e4d58
bao <thebao.developer@gmail.com>
2023-04-27 10:15:46 +0700

    docs: update styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/src/styles/base.css';
+import '@/app/styles/base.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<Component
+			className='docs'
+			{...pageProps}
+		/>
+	);
 }
