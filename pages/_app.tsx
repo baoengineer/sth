b02b60d5b8541c62b3bf2b53ee5c4d17777c80d9
bao <thebao.developer@gmail.com>
2023-04-28 16:24:18 +0700

    fix: fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/src/styles/index.css';
 import '@/app/styles/index.css';
+import '@fontsource/space-grotesk';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
